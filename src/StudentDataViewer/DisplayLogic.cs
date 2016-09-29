9f2183da58fe90c508da56e7f240110cd9558eff
kirkNovitsky <gloworms321@gmail.com>
2016-09-29 14:31:26 -0400

    Set up skeleton for display logic

---
+++ src/StudentDataViewer/DisplayLogic.cs
@@ -0,0 +1,16 @@
+﻿using StudentDataViewer.Models;
+using System;
+using System.Collections.Generic;
+
+namespace StudentDataViewer
+{
+    public class DisplayLogic
+    {
+
+        public void Display(Student studentDisplay, List<Course> courses)
+        {
+
+        }
+
+    }
+}
