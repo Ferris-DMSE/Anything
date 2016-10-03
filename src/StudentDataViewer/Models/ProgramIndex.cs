42b5ef3e30f6f84c61a48c87d5187ac1323e7d1e
EAslakson <e.k.aslakson@gmail.com>
2016-10-02 20:03:52 -0400

    Began adding search and var assignment logic for xml file

--- src/StudentDataViewer/Models/ProgramIndex.cs
+++ src/StudentDataViewer/Models/ProgramIndex.cs
@@ -20,4 +20,53 @@ namespace StudentDataViewer.Models
         public List<CourseStudent> CourseStudents;
     }
 
+    
+    public List<CourseGrade> FindCoursesForStudent(Student student)
+    {
+        List<CourseGrade> list = new List<CourseGrade>();
+        // Loop through CourseStudents
+        // Very silly
+        foreach (CourseStudent courseStudent in CourseStudent)
+        {
+            if (CourseStudent.StudentID == student.StudentID)
+            {
+                var grade = courseStudent.Grade;
+                var courseID = courseStudent.CourseID;
+                var course = FindCourseByID(courseID);
+                var courseGrade = new CourseGrade
+                {
+                   Course = course;
+                   Grade = grade;
+                }
+                list.Add(courseGrade);
+            }
+        }
+         //   if courseStudent.StudentID == student.StudentID
+         //       var grade = courseStudent.Grade
+         //       var courseID = courseStudent.CourseID
+         //       var course = FindCourseByID(courseID)
+         //       var courseGrade = new CourseGrade { ... }
+         //       list.Add(courseGrade)
+    }
+
+    public Course FindCourseByID(string courseID)
+    {
+        // The code for FindStudentByID is almost exactly the same as this pseudocode
+        foreach (Course course in Programdata)
+        {
+            if course.CourseID == courseID
+            {
+                return Course;
+            }
+        };
+
+        // Loop through Courses
+        //   if course.CourseID == courseID
+        //      return course;
+    }
+    public class CourseGrade
+    {
+        public Course Course { get; set; }
+        public string Grade { get; set; }
+    }
 }
