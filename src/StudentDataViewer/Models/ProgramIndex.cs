26723e8f3330a365f01be91a55a0b8b1e7555506
EAslakson <e.k.aslakson@gmail.com>
2016-10-03 22:20:55 -0400

    Moved FindCoursesForStudent and FindCourseByID methods into program index class, fixed syntax

--- src/StudentDataViewer/Models/ProgramIndex.cs
+++ src/StudentDataViewer/Models/ProgramIndex.cs
@@ -18,41 +18,43 @@ namespace StudentDataViewer.Models
 
         [DataMember(Order = 2)]
         public List<CourseStudent> CourseStudents;
+
+        public List<CourseGrade> FindCoursesForStudent(Student student)
+        {
+            List<CourseGrade> list = new List<CourseGrade>();
+            // Loop through CourseStudents
+            // Very silly
+            foreach (CourseStudent courseStudent in CourseStudent)
+            {
+                if (CourseStudent.StudentID == student.StudentID)
+                {
+                    var grade = courseStudent.Grade;
+                    var courseID = courseStudent.CourseID;
+                    var course = FindCourseByID(courseID);
+                    var courseGrade = new CourseGrade
+                    {
+                    Course = course:
+                    Grade = grade:
+                    }
+                    list.Add(courseGrade);
+                    return list;
+                }
+            }
+        }
+
+        public Course FindCourseByID(string courseID)
+        {
+            foreach (Course course in Programdata)
+            {
+                if (course.CourseID == courseID)
+                {
+                    return Course;
+                }
+            }
+        }
     }
 
     
-    public List<CourseGrade> FindCoursesForStudent(Student student)
-    {
-        List<CourseGrade> list = new List<CourseGrade>();
-        // Loop through CourseStudents
-        // Very silly
-        foreach (CourseStudent courseStudent in CourseStudent)
-        {
-            if (CourseStudent.StudentID == student.StudentID)
-            {
-                var grade = courseStudent.Grade;
-                var courseID = courseStudent.CourseID;
-                var course = FindCourseByID(courseID);
-                var courseGrade = new CourseGrade
-                {
-                   Course = course;
-                   Grade = grade;
-                }
-                list.Add(courseGrade);
-            }
-        }
-    }
-
-    public Course FindCourseByID(string courseID)
-    {
-        foreach (Course course in Programdata)
-        {
-            if (course.CourseID == courseID)
-            {
-                return Course;
-            }
-        }
-    }
     public class CourseGrade
     {
         public Course Course { get; set; }
