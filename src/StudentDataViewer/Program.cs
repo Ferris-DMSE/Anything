3533534f253e79c5d6dfc554c6bc8c86d1922710
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-10-04 13:25:23 -0400

    Fix build errors

--- src/StudentDataViewer/Program.cs
+++ src/StudentDataViewer/Program.cs
@@ -22,14 +22,14 @@ namespace StudentDataViewer
             {
                 FirstName = "Kirk",
                 LastName = "Novitsky",
-                StudentID = 12345,
+                StudentID = "12345",
             }, new List<Course>()
                         {
                             new Course
                             {
                                 CourseName = "Programming",
                                 CourseID = 123,
-                                CourseNumber = 321,
+                                CourseNumber = "321",
                                 Credit = 10,
                                 Semester = "Fall",
                                 Year = 2016,
