4d7eb67c62cf8f2a43b8cb409956052ce54a6f23
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-12-06 15:23:21 -0500

    Add everything but the last thing

---
+++ src/StudentDataViewer.UWP/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using StudentDataViewer.Models;
+using StudentDataViewer.Services;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace StudentDataViewer.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DataStore dataStore = new DataStore();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var programData = dataStore.LoadData();
+            StudentsListView.ItemsSource = programData.Students;
+        }
+
+        private void StudentsListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedStudent = (Student) StudentsListView.SelectedItem;
+            if (selectedStudent != null)
+            {
+                Frame.Navigate(typeof(StudentCourse), selectedStudent.StudentID, new DrillInNavigationTransitionInfo());
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            if (Frame.CanGoBack)
+            {
+                systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+                systemNavigationManager.BackRequested += SystemNavigationManagerOnBackRequested;
+            }
+            else
+            {
+                systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+        }
+
+        private void SystemNavigationManagerOnBackRequested(object sender, BackRequestedEventArgs backRequestedEventArgs)
+        {
+            if (!backRequestedEventArgs.Handled)
+            {
+                SystemNavigationManager.GetForCurrentView().BackRequested -= SystemNavigationManagerOnBackRequested;
+                backRequestedEventArgs.Handled = true;
+                Frame.GoBack();
+            }
+        }
+    }
+}
---
+++ src/StudentDataViewer.UWP/StudentCourse.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using StudentDataViewer.Models;
+using StudentDataViewer.Services;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace StudentDataViewer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StudentCourse : Page
+    {
+        private static readonly DependencyProperty studentProperty = DependencyProperty.Register(nameof(Student), typeof(Student), typeof(StudentCourse), new PropertyMetadata(null));
+        public static DependencyProperty StudentProperty => studentProperty;
+        private static readonly DependencyProperty coursesProperty = DependencyProperty.Register(nameof(Courses), typeof(List<CourseGrade>), typeof(StudentCourse), new PropertyMetadata(null));
+        public static DependencyProperty CoursesProperty => coursesProperty;
+
+        public Student Student
+        {
+            get { return (Student)GetValue(StudentProperty); }
+            set { SetValue(StudentProperty, value); }
+        }
+
+        public List<CourseGrade> Courses
+        {
+            get { return (List<CourseGrade>) GetValue(CoursesProperty); }
+            set { SetValue(CoursesProperty, value); }
+        }
+
+        public StudentCourse()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var dataStore = new DataStore();
+            var data = dataStore.LoadData();
+            var id = (string)e.Parameter;
+            var student = data.FindStudentByID(id);
+            this.Student = student;
+            var courses = data.FindCoursesForStudent(student);
+            this.Courses = courses;
+
+            var systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            if (Frame.CanGoBack)
+            {
+                systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+                systemNavigationManager.BackRequested += SystemNavigationManagerOnBackRequested;
+            }
+            else
+            {
+                systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+        }
+
+        private void SystemNavigationManagerOnBackRequested(object sender, BackRequestedEventArgs backRequestedEventArgs)
+        {
+            if (!backRequestedEventArgs.Handled)
+            {
+                SystemNavigationManager.GetForCurrentView().BackRequested -= SystemNavigationManagerOnBackRequested;
+                backRequestedEventArgs.Handled = true;
+                Frame.GoBack();
+            }
+        }
+    }
+}
